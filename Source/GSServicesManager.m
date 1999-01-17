1df9f50b5c0d1203deee30adc63de56bc42bb492
Richard Frith-MacDonald <rfm@gnu.org>
1999-01-17 08:13:03 +0000

    Another fix attempt for header
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@3568 72102866-910b-0410-8b05-ffd578937521

--- Source/GSServicesManager.m
+++ Source/GSServicesManager.m
@@ -45,7 +45,7 @@
 #include <Foundation/NSSerialization.h>
 #include <Foundation/NSPortNameServer.h>
 
-#include <gnustep/base/fast.x>
+#include <base/fast.x>
 
 #include <AppKit/NSApplication.h>
 #include <AppKit/NSPasteboard.h>
