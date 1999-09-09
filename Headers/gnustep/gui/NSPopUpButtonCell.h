fc4d042503037653532b5487bb75c5b26e9812cc
GNUstep Maintainer <gnustep-maintainer@gnu.org>
1999-09-09 03:52:13 +0000

    This commit was manufactured by cvs2svn to create tag 'nib2gmodel-0_6_0'.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/tags/nib2gmodel-0_6_0@4860 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/gui/NSPopUpButtonCell.h
+++
@@ -1,6 +0,0 @@
-#include <AppKit/NSMenuItem.h>
-
-@interface NSPopUpButtonCell : NSMenuItemCell
-{
-}
-@end
--- Model/Controller.h
+++
@@ -1,9 +0,0 @@
-#import <AppKit/AppKit.h>
-
-@interface Controller : NSObject
-{
-    id textField;
-}
-- (void)buttonPressed:(id)sender;
-- (id)window;
-@end
