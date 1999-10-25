daa7a53240830d337efd7c8e241c484f0f544d9c
Nicola Pero <n.pero@mi.flashnet.it>
1999-10-25 22:15:18 +0000

    Rewritten from scratch
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@5080 72102866-910b-0410-8b05-ffd578937521

--- Source/NSTableHeaderCell.m
+++ Source/NSTableHeaderCell.m
@@ -1,14 +1,120 @@
+/* 
+   NSTableHeaderCell.m
+
+   Copyright (C) 1999 Free Software Foundation, Inc.
+
+   Author:  Nicola Pero <n.pero@mi.flashnet.it>
+   Date: 1999
+   
+   This file is part of the GNUstep GUI Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/ 
+
 #include <AppKit/NSTableHeaderCell.h>
 #include <AppKit/NSColor.h>
+#include <AppKit/NSGraphics.h>
+#include <AppKit/NSImage.h>
+
+// Cache the colors
+static NSColor *bgCol;
+static NSColor *hbgCol;
+static NSColor *clearCol = nil;
 
 @implementation NSTableHeaderCell
-- (void) drawInteriorWithFrame: (NSRect)cellFrame
-		        inView: (NSView *)controlView
 {
-  [controlView lockFocus];
-  [[NSColor controlShadowColor] set];
-  NSRectFill(cellFrame);
-  [super drawInteriorWithFrame: cellFrame inView: controlView];
-  [controlView unlockFocus];
+}
+// Default appearance of NSTableHeaderCell
+- (id) initTextCell: (NSString *)aString
+{
+  [super initTextCell: aString];
+
+  [self setAlignment: NSCenterTextAlignment];
+  [self setTextColor: [NSColor windowFrameTextColor]];
+  [self setBackgroundColor: [NSColor controlShadowColor]];
+  [self setBordered: NO];
+  [self setBezeled: NO];
+  draw_background = YES;
+
+  return self;
+}
+// Override drawInteriorWithFrame:inView: to be able 
+// to display images as NSCell does
+- (void) drawInteriorWithFrame: (NSRect)cellFrame 
+			inView: (NSView*)controlView
+{
+  switch ([self type])
+    {
+    case NSTextCellType:
+      [super drawInteriorWithFrame: cellFrame inView: controlView];
+      break;
+      
+    case NSImageCellType:
+      //
+      // Taken (with modifications) from NSCell
+      //
+
+      // Initialize static colors if needed
+      if (clearCol == nil)
+	{
+	  bgCol = RETAIN([NSColor selectedControlColor]);
+	  hbgCol = RETAIN([NSColor controlBackgroundColor]);
+	  clearCol = RETAIN([NSColor clearColor]);
+	}
+      // Prepare to draw
+      cellFrame = [self drawingRectForBounds: cellFrame];
+      [controlView lockFocus];
+      // Deal with the background
+      if ([self isOpaque])
+	{
+	  NSColor *bg;
+	  
+	  if (cell_highlighted)
+	    bg = bgCol;
+	  else
+	    bg = hbgCol;
+	  [bg set];
+	  NSRectFill (cellFrame);
+	  if (cell_image)
+	    [cell_image setBackgroundColor: bg];
+	}
+      else
+	{
+	  if (cell_image)
+	    [cell_image setBackgroundColor: clearCol];
+	}
+      // Draw the image
+      if (cell_image)
+	{
+	  NSSize size;
+	  NSPoint position;
+	  
+	  size = [cell_image size];
+	  position.x = MAX (NSMidX (cellFrame) - (size.width/2.), 0.);
+	  position.y = MAX (NSMidY (cellFrame) - (size.height/2.), 0.);
+	  if ([control_view isFlipped])
+	    position.y += size.height;
+	  [cell_image compositeToPoint: position operation: NSCompositeCopy];
+	}
+      // End the drawing
+      [controlView unlockFocus];
+      break;
+      
+    case NSNullCellType:
+      break;
+    }
 }
 @end
