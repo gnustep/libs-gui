ca3979a1a38c2566b658fd1e82973a9edee4d7e5
Richard Frith-MacDonald <rfm@gnu.org>
1999-12-20 12:00:26 +0000

    Tiny prototype fix
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@5585 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/gui/NSBrowser.h
+++ Headers/gnustep/gui/NSBrowser.h
@@ -81,152 +81,152 @@
 //
 // Setting the Delegate 
 //
-- (id)delegate;
-- (void)setDelegate:(id)anObject;
+- (id) delegate;
+- (void) setDelegate: (id)anObject;
 
 //
 // Target and Action 
 //
-- (SEL)doubleAction;
-- (BOOL)sendAction;
-- (void)setDoubleAction:(SEL)aSelector;
+- (SEL) doubleAction;
+- (BOOL) sendAction;
+- (void) setDoubleAction: (SEL)aSelector;
 
 //
 // Setting Component Classes 
 //
-+ (Class)cellClass;
-- (id)cellPrototype;
-- (Class)matrixClass;
-- (void)setCellClass:(Class)classId;
-- (void)setCellPrototype:(NSCell *)aCell;
-- (void)setMatrixClass:(Class)classId;
++ (Class) cellClass;
+- (id) cellPrototype;
+- (Class) matrixClass;
+- (void) setCellClass: (Class)classId;
+- (void) setCellPrototype: (NSCell *)aCell;
+- (void) setMatrixClass: (Class)classId;
 
 //
 // Setting NSBrowser Behavior 
 //
-- (BOOL)reusesColumns;
-- (void)setReusesColumns:(BOOL)flag;
-- (void)setTakesTitleFromPreviousColumn:(BOOL)flag;
-- (BOOL)takesTitleFromPreviousColumn;
+- (BOOL) reusesColumns;
+- (void) setReusesColumns: (BOOL)flag;
+- (void) setTakesTitleFromPreviousColumn: (BOOL)flag;
+- (BOOL) takesTitleFromPreviousColumn;
 
 //
 // Allowing Different Types of Selection 
 //
-- (BOOL)allowsBranchSelection;
-- (BOOL)allowsEmptySelection;
-- (BOOL)allowsMultipleSelection;
-- (void)setAllowsBranchSelection:(BOOL)flag;
-- (void)setAllowsEmptySelection:(BOOL)flag;
-- (void)setAllowsMultipleSelection:(BOOL)flag;
+- (BOOL) allowsBranchSelection;
+- (BOOL) allowsEmptySelection;
+- (BOOL) allowsMultipleSelection;
+- (void) setAllowsBranchSelection: (BOOL)flag;
+- (void) setAllowsEmptySelection: (BOOL)flag;
+- (void) setAllowsMultipleSelection: (BOOL)flag;
 
 //
 // Setting Arrow Key Behavior
 //
-- (BOOL)acceptsArrowKeys;
-- (BOOL)sendsActionOnArrowKeys;
-- (void)setAcceptsArrowKeys:(BOOL)flag;
-- (void)setSendsActionOnArrowKeys:(BOOL)flag;
+- (BOOL) acceptsArrowKeys;
+- (BOOL) sendsActionOnArrowKeys;
+- (void) setAcceptsArrowKeys: (BOOL)flag;
+- (void) setSendsActionOnArrowKeys: (BOOL)flag;
 
 //
 // Showing a Horizontal Scroller 
 //
-- (void)setHasHorizontalScroller:(BOOL)flag;
-- (BOOL)hasHorizontalScroller;
+- (void) setHasHorizontalScroller: (BOOL)flag;
+- (BOOL) hasHorizontalScroller;
 
 //
 // Setting the NSBrowser's Appearance 
 //
-- (int)maxVisibleColumns;
-- (int)minColumnWidth;
-- (BOOL)separatesColumns;
-- (void)setMaxVisibleColumns:(int)columnCount;
-- (void)setMinColumnWidth:(int)columnWidth;
-- (void)setSeparatesColumns:(BOOL)flag;
+- (int) maxVisibleColumns;
+- (int) minColumnWidth;
+- (BOOL) separatesColumns;
+- (void) setMaxVisibleColumns: (int)columnCount;
+- (void) setMinColumnWidth: (int)columnWidth;
+- (void) setSeparatesColumns: (BOOL)flag;
 
 //
 // Manipulating Columns 
 //
-- (void)addColumn;
-- (int)columnOfMatrix:(NSMatrix *)matrix;
-- (void)displayAllColumns;
-- (void)displayColumn:(int)column;
-- (int)firstVisibleColumn;
-- (BOOL)isLoaded;
-- (int)lastColumn;
-- (int)lastVisibleColumn;
-- (void)loadColumnZero;
-- (int)numberOfVisibleColumns;
-- (void)reloadColumn:(int)column;
-- (void)selectAll:(id)sender;
-- (int)selectedColumn;
-- (int) selectedRowInColumn: (int)column;
-- (void)setLastColumn:(int)column;
-- (void)validateVisibleColumns;
+- (void) addColumn;
+- (int) columnOfMatrix: (NSMatrix *)matrix;
+- (void) displayAllColumns;
+- (void) displayColumn: (int)column;
+- (int) firstVisibleColumn;
+- (BOOL) isLoaded;
+- (int) lastColumn;
+- (int) lastVisibleColumn;
+- (void) loadColumnZero;
+- (int) numberOfVisibleColumns;
+- (void) reloadColumn: (int)column;
+- (void) selectAll: (id)sender;
+- (int) selectedColumn;
+- (int) selectedRow: (int)row nColumn: (int)column;
+- (void) setLastColumn: (int)column;
+- (void) validateVisibleColumns;
 
 //
 // Manipulating Column Titles 
 //
-- (void)drawTitle:(NSString *)title
-	   inRect:(NSRect)aRect
-	 ofColumn:(int)column;
-- (BOOL)isTitled;
-- (void)setTitled:(BOOL)flag;
-- (void)setTitle:(NSString *)aString
-	ofColumn:(int)column;
-- (NSRect)titleFrameOfColumn:(int)column;
-- (float)titleHeight;
-- (NSString *)titleOfColumn:(int)column;
+- (void) drawTitle: (NSString *)title
+	    inRect: (NSRect)aRect
+	  ofColumn: (int)column;
+- (BOOL) isTitled;
+- (void) setTitled: (BOOL)flag;
+- (void) setTitle: (NSString *)aString
+	 ofColumn: (int)column;
+- (NSRect) titleFrameOfColumn: (int)column;
+- (float) titleHeight;
+- (NSString *) titleOfColumn: (int)column;
 
 //
 // Scrolling an NSBrowser 
 //
-- (void)scrollColumnsLeftBy:(int)shiftAmount;
-- (void)scrollColumnsRightBy:(int)shiftAmount;
-- (void)scrollColumnToVisible:(int)column;
-- (void)scrollViaScroller:(NSScroller *)sender;
-- (void)updateScroller;
+- (void) scrollColumnsLeftBy: (int)shiftAmount;
+- (void) scrollColumnsRightBy: (int)shiftAmount;
+- (void) scrollColumnToVisible: (int)column;
+- (void) scrollViaScroller: (NSScroller *)sender;
+- (void) updateScroller;
 
 //
 // Event Handling 
 //
-- (void)doClick:(id)sender;
-- (void)doDoubleClick:(id)sender;
+- (void) doClick: (id)sender;
+- (void) doDoubleClick: (id)sender;
 
 //
 // Getting Matrices and Cells 
 //
-- (id)loadedCellAtRow:(int)row
-	       column:(int)column;
-- (NSMatrix *)matrixInColumn:(int)column;
-- (id)selectedCell;
-- (id)selectedCellInColumn:(int)column;
-- (NSArray *)selectedCells;
+- (id) loadedCellAtRow: (int)row
+	        column: (int)column;
+- (NSMatrix *) matrixInColumn: (int)column;
+- (id) selectedCell;
+- (id) selectedCellInColumn: (int)column;
+- (NSArray *) selectedCells;
 
 //
 // Getting Column Frames 
 //
-- (NSRect)frameOfColumn:(int)column;
-- (NSRect)frameOfInsideOfColumn:(int)column;
+- (NSRect) frameOfColumn: (int)column;
+- (NSRect) frameOfInsideOfColumn: (int)column;
 
 //
 // Manipulating Paths 
 //
-- (NSString *)path;
-- (NSString *)pathSeparator;
-- (NSString *)pathToColumn:(int)column;
-- (BOOL)setPath:(NSString *)path;
-- (void)setPathSeparator:(NSString *)aString;
+- (NSString *) path;
+- (NSString *) pathSeparator;
+- (NSString *) pathToColumn: (int)column;
+- (BOOL) setPath: (NSString *)path;
+- (void) setPathSeparator: (NSString *)aString;
 
 //
 // Arranging an NSBrowser's Components 
 //
-- (void)tile;
+- (void) tile;
 
 //
 // NSCoding protocol
 //
-- (void)encodeWithCoder:aCoder;
-- initWithCoder:aDecoder;
+- (void) encodeWithCoder: aCoder;
+- (id) initWithCoder: aDecoder;
 
 @end
 
@@ -236,27 +236,20 @@
 //
 @interface NSObject (NSBrowserDelegate)
 
-- (void)browser:(NSBrowser *)sender
-  createRowsForColumn:(int)column
-  inMatrix:(NSMatrix *)matrix;
-- (BOOL)browser:(NSBrowser *)sender
-  isColumnValid:(int)column;
-- (int)browser:(NSBrowser *)sender
-  numberOfRowsInColumn:(int)column;
-- (BOOL)browser:(NSBrowser *)sender
-selectCellWithString:(NSString *)title
-       inColumn:(int)column;
-- (BOOL)browser:(NSBrowser *)sender
-      selectRow:(int)row
-       inColumn:(int)column;
-- (NSString *)browser:(NSBrowser *)sender
-	titleOfColumn:(int)column;
-- (void)browser:(NSBrowser *)sender
-  willDisplayCell:(id)cell
-  atRow:(int)row
-  column:(int)column;
-- (void)browserDidScroll:(NSBrowser *)sender;
-- (void)browserWillScroll:(NSBrowser *)sender;
+- (void) browser: (NSBrowser *)sender createRowsForColumn: (int)column
+  inMatrix: (NSMatrix *)matrix;
+- (BOOL) browser: (NSBrowser *)sender isColumnValid: (int)column;
+- (int) browser: (NSBrowser *)sender numberOfRowsInColumn: (int)column;
+- (BOOL) browser: (NSBrowser *)sender selectCellWithString: (NSString *)title
+  inColumn: (int)column;
+- (BOOL) browser: (NSBrowser *)sender selectRow: (int)row inColumn: (int)column;
+- (NSString *) browser: (NSBrowser *)sender titleOfColumn: (int)column;
+- (void) browser: (NSBrowser *)sender
+ willDisplayCell: (id)cell
+	   atRow: (int)row
+	  column: (int)column;
+- (void) browserDidScroll: (NSBrowser *)sender;
+- (void) browserWillScroll: (NSBrowser *)sender;
 
 @end
 
