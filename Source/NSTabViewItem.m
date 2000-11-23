71527b54da1f3600e2230014fb572f24e29f37ec
Nicola Pero <n.pero@mi.flashnet.it>
2000-11-23 10:17:47 +0000

    Patch from Georg Fleischmann removing release in excess; include file to avoid compiler warnings; added copyright notice
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@8182 72102866-910b-0410-8b05-ffd578937521

--- Source/NSTabViewItem.m
+++ Source/NSTabViewItem.m
@@ -1,8 +1,32 @@
+/*
+   NSTabViewItem.m
+
+   Copyright (C) 2000 Free Software Foundation, Inc.
+
+   This file is part of the GNUstep GUI Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
 #include <AppKit/NSColor.h>
 #include <AppKit/NSFont.h>
 #include <AppKit/NSImage.h>
 #include <AppKit/NSTabViewItem.h>
 #include <AppKit/PSOperators.h>
+#include <AppKit/NSGraphics.h>
 
 @implementation NSTabViewItem
 - (id) initWithIdentifier:(id)identifier
@@ -69,9 +93,6 @@
 
 - (void)setView:(NSView *)view
 {
-  if (item_view)
-    TEST_RELEASE(item_view);
-
   ASSIGN(item_view, view);
 }
 
