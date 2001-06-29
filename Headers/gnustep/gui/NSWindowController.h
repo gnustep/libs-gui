e257334240135e125102dfb1ef0f2bc6e93e983c
Nicola Pero <n.pero@mi.flashnet.it>
2001-06-29 10:17:38 +0000

    Indendation changes; removed a useless @class NSArray; declaration
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@10268 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/gui/NSWindowController.h
+++ Headers/gnustep/gui/NSWindowController.h
@@ -28,7 +28,7 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/NSNibDeclarations.h>
 
-@class NSWindow, NSDocument, NSArray;
+@class NSWindow, NSDocument;
 
 @interface NSWindowController : NSObject <NSCoding>
 {
@@ -39,39 +39,40 @@
     NSDocument          *_document;
     NSArray             *_topLevelObjects;
     id                  _owner;
-    struct ___wcFlags {
-        unsigned int shouldCloseDocument:1;
-        unsigned int shouldCascade:1;
-        unsigned int nibIsLoaded:1;
-        unsigned int RESERVED:29;
+    struct ___wcFlags 
+    {
+      unsigned int shouldCloseDocument:1;
+      unsigned int shouldCascade:1;
+      unsigned int nibIsLoaded:1;
+      unsigned int RESERVED:29;
     } _wcFlags;
     void                *_reserved1;
     void                *_reserved2;
 }
 
-- (id)initWithWindowNibName:(NSString *)windowNibName;  // self is the owner
-- (id)initWithWindowNibName:(NSString *)windowNibName owner:(id)owner;
-- (id)initWithWindow:(NSWindow *)window;
+- (id) initWithWindowNibName: (NSString *)windowNibName;  // self is the owner
+- (id) initWithWindowNibName: (NSString *)windowNibName  owner: (id)owner;
+- (id) initWithWindow: (NSWindow *)window;
 
-- (NSString *)windowNibName;
-- (id)owner;
-- (void)setDocument:(NSDocument *)document;
-- (id)document;
-- (void)setWindowFrameAutosaveName:(NSString *)name;
-- (NSString *)windowFrameAutosaveName;
-- (void)setShouldCloseDocument:(BOOL)flag;
-- (BOOL)shouldCloseDocument;
-- (void)setShouldCascadeWindows:(BOOL)flag;
-- (BOOL)shouldCascadeWindows;
-- (void)close;
-- (NSWindow *)window;
-- (IBAction)showWindow:(id)sender;
-- (NSString *)windowTitleForDocumentDisplayName:(NSString *)displayName;
-- (BOOL)isWindowLoaded;
-- (void)windowDidLoad;
-- (void)windowWillLoad;
-- (void)loadWindow;
+- (NSString *) windowNibName;
+- (id) owner;
+- (void) setDocument: (NSDocument *)document;
+- (id) document;
+- (void) setWindowFrameAutosaveName: (NSString *)name;
+- (NSString *) windowFrameAutosaveName;
+- (void) setShouldCloseDocument: (BOOL)flag;
+- (BOOL) shouldCloseDocument;
+- (void) setShouldCascadeWindows: (BOOL)flag;
+- (BOOL) shouldCascadeWindows;
+- (void) close;
+- (NSWindow *) window;
+- (IBAction) showWindow: (id)sender;
+- (NSString *) windowTitleForDocumentDisplayName: (NSString *)displayName;
+- (BOOL) isWindowLoaded;
+- (void) windowDidLoad;
+- (void) windowWillLoad;
+- (void) loadWindow;
 
 @end
 
-#endif
+#endif /* _GNUstep_H_NSWindowController */
