a3e838fd0f421e53ab995ab00af61327043ebb12
Fred Kiefer <fredkiefer@gmx.de>
2001-08-18 15:14:55 +0000

    Declaration of this class and of the protocols NSInputServerMouseTracker and NSInputServiceProvider.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@10714 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/gnustep/gui/NSInputServer.h
@@ -0,0 +1,78 @@
+/*                                                    -*-objc-*-
+   NSInputServer.h
+
+   Copyright (C) 2001 Free Software Foundation, Inc.
+
+   Author: Fred Kiefer <FredKiefer@gmx.de>
+   Date: August 2001
+
+   This file is part of the GNUstep GUI Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, write to the Free Software Foundation,
+   59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/ 
+
+#ifndef _GNUstep_H_NSInputServer
+#define _GNUstep_H_NSInputServer
+
+#include <objc/Protocol.h>
+#include <Foundation/NSGeometry.h>
+#include <Foundation/NSString.h>
+
+@protocol NSInputServerMouseTracker
+- (BOOL) mouseDownOnCharacterIndex: (unsigned)index
+		      atCoordinate: (NSPoint)point
+		      withModifier: (unsigned int)flags
+			    client: (id)sender;
+- (BOOL) mouseDraggedOnCharacterIndex: (unsigned)index
+			 atCoordinate: (NSPoint)point
+			 withModifier: (unsigned int)flags
+			       client: (id)sender;
+- (void) mouseUpOnCharacterIndex: (unsigned)index
+		    atCoordinate: (NSPoint)point
+		    withModifier: (unsigned int)flags
+			  client: (id)sender;
+@end
+
+@protocol NSInputServiceProvider
+- (void) activeConversationChanged: (id)sender
+		 toNewConversation: (long)newConversation;
+- (void) activeConversationWillChange: (id)sender
+		  fromOldConversation: (long)oldConversation;
+- (BOOL) canBeDisabled;
+- (void) doCommandBySelector: (SEL)aSelector
+		      client: (id)sender;
+- (void) inputClientBecomeActive: (id)sender;
+- (void) inputClientDisabled: (id)sender;
+- (void) inputClientEnabled: (id)sender;
+- (void) inputClientResignActive: (id)sender;
+- (void) insertText: (id)aString
+	     client: (id)sender;
+- (void) markedTextAbandoned: (id)sender;
+- (void) markedTextSelectionChanged: (NSRange)newSelection
+			     client: (id)sender;
+- (void) terminate: (id)sender;
+- (BOOL) wantsToDelayTextChangeNotifications;
+- (BOOL) wantsToHandleMouseEvents;
+- (BOOL) wantsToInterpretAllKeystrokes;
+@end
+
+@interface NSInputServer: NSObject <NSInputServerMouseTracker, NSInputServiceProvider>
+
+- (id) initWithDelegate: (id)aDelegate
+		   name: (NSString *)name;
+@end
+
+#endif //_GNUstep_H_NSInputServer
