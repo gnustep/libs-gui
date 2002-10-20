90879f7d742bcc69adb6f9cb0465b85b9211fd76
Richard Frith-Macdonald <rfm@gnu.org>
2002-10-20 18:23:10 +0000

    Minor consistency fix
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@14802 72102866-910b-0410-8b05-ffd578937521

--- Headers/gnustep/gui/NSComboBox.h
+++ Headers/gnustep/gui/NSComboBox.h
@@ -34,79 +34,79 @@
 {
 }
 
-- (BOOL)hasVerticalScroller;
-- (void)setHasVerticalScroller:(BOOL)flag;
+- (BOOL) hasVerticalScroller;
+- (void) setHasVerticalScroller: (BOOL)flag;
 
-- (NSSize)intercellSpacing;
-- (void)setIntercellSpacing:(NSSize)aSize;
+- (NSSize) intercellSpacing;
+- (void) setIntercellSpacing: (NSSize)aSize;
 
-- (float)itemHeight;
-- (void)setItemHeight:(float)itemHeight;
+- (float) itemHeight;
+- (void) setItemHeight: (float)itemHeight;
 
-- (int)numberOfVisibleItems;
-- (void)setNumberOfVisibleItems:(int)visibleItems;
+- (int) numberOfVisibleItems;
+- (void) setNumberOfVisibleItems: (int)visibleItems;
 
-- (void)reloadData;
-- (void)noteNumberOfItemsChanged;
+- (void) reloadData;
+- (void) noteNumberOfItemsChanged;
 
-- (BOOL)usesDataSource;
-- (void)setUsesDataSource:(BOOL)flag;
+- (BOOL) usesDataSource;
+- (void) setUsesDataSource: (BOOL)flag;
 
-- (void)scrollItemAtIndexToTop:(int)index;
-- (void)scrollItemAtIndexToVisible:(int)index;
+- (void) scrollItemAtIndexToTop: (int)index;
+- (void) scrollItemAtIndexToVisible: (int)index;
 
-- (void)selectItemAtIndex:(int)index;
-- (void)deselectItemAtIndex:(int)index;
-- (int)indexOfSelectedItem;
-- (int)numberOfItems;
+- (void) selectItemAtIndex: (int)index;
+- (void) deselectItemAtIndex: (int)index;
+- (int) indexOfSelectedItem;
+- (int) numberOfItems;
 
 /* These two methods can only be used when usesDataSource is YES */
-- (id)dataSource;
-- (void)setDataSource:(id)aSource;
+- (id) dataSource;
+- (void) setDataSource: (id)aSource;
 
 /* These methods can only be used when usesDataSource is NO */
-- (void)addItemWithObjectValue:(id)object;
-- (void)addItemsWithObjectValues:(NSArray *)objects;
-- (void)insertItemWithObjectValue:(id)object atIndex:(int)index;
-- (void)removeItemWithObjectValue:(id)object;
-- (void)removeItemAtIndex:(int)index;
-- (void)removeAllItems;
-- (void)selectItemWithObjectValue:(id)object;
-- (id)itemObjectValueAtIndex:(int)index;
-- (id)objectValueOfSelectedItem;
-- (int)indexOfItemWithObjectValue:(id)object;
-- (NSArray *)objectValues;
+- (void) addItemWithObjectValue: (id)object;
+- (void) addItemsWithObjectValues: (NSArray *)objects;
+- (void) insertItemWithObjectValue: (id)object atIndex:(int)index;
+- (void) removeItemWithObjectValue: (id)object;
+- (void) removeItemAtIndex: (int)index;
+- (void) removeAllItems;
+- (void) selectItemWithObjectValue: (id)object;
+- (id) itemObjectValueAtIndex: (int)index;
+- (id) objectValueOfSelectedItem;
+- (int) indexOfItemWithObjectValue: (id)object;
+- (NSArray *) objectValues;
 
 #ifndef	STRICT_OPENSTEP
 /* text completion */
-- (void)setCompletes:(BOOL)completes;
-- (BOOL)completes;
+- (void) setCompletes: (BOOL)completes;
+- (BOOL) completes;
 #endif
 
 @end
 
 @interface NSObject (NSComboBoxDataSource)
-- (int)numberOfItemsInComboBox:(NSComboBox *)aComboBox;
-- (id)comboBox:(NSComboBox *)aComboBox objectValueForItemAtIndex:(int)index;
-- (unsigned int)comboBox:(NSComboBox *)aComboBox
-indexOfItemWithStringValue:(NSString *)string;
+- (int) numberOfItemsInComboBox: (NSComboBox *)aComboBox;
+- (id) comboBox: (NSComboBox *)aComboBox objectValueForItemAtIndex:(int)index;
+- (unsigned int) comboBox: (NSComboBox *)aComboBox
+indexOfItemWithStringValue: (NSString *)string;
 #ifndef	STRICT_OPENSTEP
 /* text completion */
-- (NSString *)comboBox:(NSComboBox *)aComboBox 
-       completedString:(NSString *)aString;
+- (NSString *) comboBox: (NSComboBox *)aComboBox 
+	completedString: (NSString *)aString;
 #endif
 @end
 
 @interface NSObject (NSComboBoxNotifications)
-- (void)comboBoxWillPopUp:(NSNotification *)notification;
-- (void)comboBoxWillDismiss:(NSNotification *)notification;
-- (void)comboBoxSelectionDidChange:(NSNotification *)notification;
-- (void)comboBoxSelectionIsChanging:(NSNotification *)notification;
+- (void) comboBoxWillPopUp: (NSNotification *)notification;
+- (void) comboBoxWillDismiss: (NSNotification *)notification;
+- (void) comboBoxSelectionDidChange: (NSNotification *)notification;
+- (void) comboBoxSelectionIsChanging: (NSNotification *)notification;
 @end
 
-NSString *NSComboBoxWillPopUpNotification;
-NSString *NSComboBoxWillDismissNotification;
-NSString *NSComboBoxSelectionDidChangeNotification;
-NSString *NSComboBoxSelectionIsChangingNotification;
+APPKIT_EXPORT	NSString *NSComboBoxWillPopUpNotification;
+APPKIT_EXPORT	NSString *NSComboBoxWillDismissNotification;
+APPKIT_EXPORT	NSString *NSComboBoxSelectionDidChangeNotification;
+APPKIT_EXPORT	NSString *NSComboBoxSelectionIsChangingNotification;
 
 #endif /* _GNUstep_H_NSComboBox */
