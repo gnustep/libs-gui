514ee3f92798aa5adb67d7f2e1e9a769d6ff6ed1
Fred Kiefer <fredkiefer@gmx.de>
2002-11-24 01:06:46 +0000

    Regenerated with bision 1.35
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@15068 72102866-910b-0410-8b05-ffd578937521

--- TextConverters/RTF/rtfGrammer.tab.h
+++ TextConverters/RTF/rtfGrammer.tab.h
@@ -1,94 +1,86 @@
+#ifndef BISON_RTFGRAMMER_TAB_H
+# define BISON_RTFGRAMMER_TAB_H
+
+#ifndef YYSTYPE
 typedef union {
 	int		number;
 	const char	*text;
 	RTFcmd		cmd;
-} YYSTYPE;
-
-#ifndef YYLTYPE
-typedef
-  struct yyltype
-    {
-      int timestamp;
-      int first_line;
-      int first_column;
-      int last_line;
-      int last_column;
-      char *text;
-   }
-  yyltype;
-
-#define YYLTYPE yyltype
+} yystype;
+# define YYSTYPE yystype
+# define YYSTYPE_IS_TRIVIAL 1
 #endif
+# define	RTFtext	257
+# define	RTFstart	258
+# define	RTFansi	259
+# define	RTFmac	260
+# define	RTFpc	261
+# define	RTFpca	262
+# define	RTFignore	263
+# define	RTFinfo	264
+# define	RTFstylesheet	265
+# define	RTFfootnote	266
+# define	RTFheader	267
+# define	RTFfooter	268
+# define	RTFpict	269
+# define	RTFplain	270
+# define	RTFparagraph	271
+# define	RTFdefaultParagraph	272
+# define	RTFrow	273
+# define	RTFcell	274
+# define	RTFtabulator	275
+# define	RTFemdash	276
+# define	RTFendash	277
+# define	RTFemspace	278
+# define	RTFenspace	279
+# define	RTFbullet	280
+# define	RTFlquote	281
+# define	RTFrquote	282
+# define	RTFldblquote	283
+# define	RTFrdblquote	284
+# define	RTFred	285
+# define	RTFgreen	286
+# define	RTFblue	287
+# define	RTFcolorbg	288
+# define	RTFcolorfg	289
+# define	RTFcolortable	290
+# define	RTFfont	291
+# define	RTFfontSize	292
+# define	RTFpaperWidth	293
+# define	RTFpaperHeight	294
+# define	RTFmarginLeft	295
+# define	RTFmarginRight	296
+# define	RTFmarginTop	297
+# define	RTFmarginButtom	298
+# define	RTFfirstLineIndent	299
+# define	RTFleftIndent	300
+# define	RTFrightIndent	301
+# define	RTFalignCenter	302
+# define	RTFalignJustified	303
+# define	RTFalignLeft	304
+# define	RTFalignRight	305
+# define	RTFlineSpace	306
+# define	RTFspaceAbove	307
+# define	RTFstyle	308
+# define	RTFbold	309
+# define	RTFitalic	310
+# define	RTFunderline	311
+# define	RTFunderlineStop	312
+# define	RTFsubscript	313
+# define	RTFsuperscript	314
+# define	RTFtabstop	315
+# define	RTFfcharset	316
+# define	RTFfprq	317
+# define	RTFcpg	318
+# define	RTFOtherStatement	319
+# define	RTFfontListStart	320
+# define	RTFfamilyNil	321
+# define	RTFfamilyRoman	322
+# define	RTFfamilySwiss	323
+# define	RTFfamilyModern	324
+# define	RTFfamilyScript	325
+# define	RTFfamilyDecor	326
+# define	RTFfamilyTech	327
 
-#define	RTFtext	258
-#define	RTFstart	259
-#define	RTFansi	260
-#define	RTFmac	261
-#define	RTFpc	262
-#define	RTFpca	263
-#define	RTFignore	264
-#define	RTFinfo	265
-#define	RTFstylesheet	266
-#define	RTFfootnote	267
-#define	RTFheader	268
-#define	RTFfooter	269
-#define	RTFpict	270
-#define	RTFplain	271
-#define	RTFparagraph	272
-#define	RTFdefaultParagraph	273
-#define	RTFrow	274
-#define	RTFcell	275
-#define	RTFtabulator	276
-#define	RTFemdash	277
-#define	RTFendash	278
-#define	RTFemspace	279
-#define	RTFenspace	280
-#define	RTFbullet	281
-#define	RTFlquote	282
-#define	RTFrquote	283
-#define	RTFldblquote	284
-#define	RTFrdblquote	285
-#define	RTFred	286
-#define	RTFgreen	287
-#define	RTFblue	288
-#define	RTFcolorbg	289
-#define	RTFcolorfg	290
-#define	RTFcolortable	291
-#define	RTFfont	292
-#define	RTFfontSize	293
-#define	RTFpaperWidth	294
-#define	RTFpaperHeight	295
-#define	RTFmarginLeft	296
-#define	RTFmarginRight	297
-#define	RTFmarginTop	298
-#define	RTFmarginButtom	299
-#define	RTFfirstLineIndent	300
-#define	RTFleftIndent	301
-#define	RTFrightIndent	302
-#define	RTFalignCenter	303
-#define	RTFalignJustified	304
-#define	RTFalignLeft	305
-#define	RTFalignRight	306
-#define	RTFlineSpace	307
-#define	RTFspaceAbove	308
-#define	RTFstyle	309
-#define	RTFbold	310
-#define	RTFitalic	311
-#define	RTFunderline	312
-#define	RTFunderlineStop	313
-#define	RTFsubscript	314
-#define	RTFsuperscript	315
-#define	RTFtabstop	316
-#define	RTFfcharset	317
-#define	RTFfprq	318
-#define	RTFcpg	319
-#define	RTFOtherStatement	320
-#define	RTFfontListStart	321
-#define	RTFfamilyNil	322
-#define	RTFfamilyRoman	323
-#define	RTFfamilySwiss	324
-#define	RTFfamilyModern	325
-#define	RTFfamilyScript	326
-#define	RTFfamilyDecor	327
-#define	RTFfamilyTech	328
 
+#endif /* not BISON_RTFGRAMMER_TAB_H */
