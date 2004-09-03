e8ce1bfa9f6b626af71d8a77249dc0ddb6de4e7b
Alexander Malmberg <alexander@malmberg.org>
2004-09-03 23:10:13 +0000

    Remove spurious ';'.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@19985 72102866-910b-0410-8b05-ffd578937521

--- Source/GSToolbarView.m
+++ Source/GSToolbarView.m
@@ -858,7 +858,7 @@ static void initSystemExtensionsColors(void)
 - (int) _insertionIndexAtPoint: (NSPoint)location
 {
   id hitView = [self hitTest: location];
-  NSRect hitViewFrame = [hitView frame];;
+  NSRect hitViewFrame = [hitView frame];
   int index;
   
   if ((hitView != nil)
