52469c268ec7fd95080dc0ae9247571072809cd3
Adam Fedor <fedor@gnu.org>
2005-07-01 20:54:03 +0000

    Add/fix copyright/licenses.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@21393 72102866-910b-0410-8b05-ffd578937521

--- Tools/wgetopt.h
+++ Tools/wgetopt.h
@@ -1,3 +1,23 @@
+/* wgetopt
+
+   Copyright (C) 2005 Free Software Foundation, Inc.
+
+   This file is part of the GNU Objective C User Interface library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111 USA.
+   */
 #if (defined __MINGW__)
 /* A simple implementation of getopt() */
 static int
