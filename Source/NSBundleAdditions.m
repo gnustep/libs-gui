8952b82d93a8c92fecdd06469bb225be01b3f4b9
Richard Frith-MacDonald <rfm@gnu.org>
2006-05-10 13:06:57 +0000

    Fix for loading where the files owner is in a library, framework or bundle, but the Gorm/NIB file is in the application resources.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@22887 72102866-910b-0410-8b05-ffd578937521

--- Source/NSBundleAdditions.m
+++ Source/NSBundleAdditions.m
@@ -344,7 +344,13 @@ Class gmodel_class(void)
     }
   table = [NSDictionary dictionaryWithObject: owner forKey: @"NSOwner"];
   bundle = [self bundleForClass: [owner class]];
-  if (bundle == nil)
+  if (bundle != nil && [bundle loadNibFile: aNibName
+			 externalNameTable: table
+				  withZone: [owner zone]] == YES)
+    {
+      return YES;
+    }
+  else
     {
       bundle = [self mainBundle];
     }
