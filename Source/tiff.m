7afdd32ac1f1509f4f1e4a060825b61a079b9b97
Adam Fedor <fedor@gnu.org>
2008-06-10 04:01:49 +0000

    Revert library license version to 2 until applications can be converted
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@26619 72102866-910b-0410-8b05-ffd578937521

--- Source/tiff.m
+++ Source/tiff.m
@@ -15,7 +15,7 @@
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
+   version 2 of the License, or (at your option) any later version.
 
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -590,7 +590,7 @@ NSTiffGetColormap(TIFF* image)
 int NSTiffIsCodecConfigured(unsigned int codec)
 {
 #if (TIFFLIB_VERSION >= 20041016)
-  // starting with version 3.7.0 we can ask libtiff what it is configured to do
+  // starting with version 2.7.0 we can ask libtiff what it is configured to do
   return TIFFIsCODECConfigured(codec);
 #else
   // we check the tiffconf.h
