81508c17ff75bd84ec202c71b67443adfd458407
Eric Wasylishen <ewasylishen@gmail.com>
2011-07-07 05:05:37 +0000

    * Tests/gui/NSView/NSView_autoresize_and_rounding.m: More autoresizing test cases
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@33472 72102866-910b-0410-8b05-ffd578937521

--- Tests/gui/NSView/NSView_autoresize_and_rounding.m
+++ Tests/gui/NSView/NSView_autoresize_and_rounding.m
@@ -42,19 +42,71 @@ int main(int argc, char **argv)
 
 	[NSApplication sharedApplication];
 	window = [[NSWindow alloc] initWithContentRect: NSMakeRect(100,100,100,100)
-		styleMask: NSClosableWindowMask
+		styleMask: NSBorderlessWindowMask
 		backing: NSBackingStoreRetained
 		defer: YES];
 
 	view1 = [[NSView alloc] initWithFrame: NSMakeRect(10,10,10,10)];
+	[[window contentView] addSubview: view1];
+
+	/**
+	 * Basic autoresizing test
+	 */
+
+	// No autosizing
+
+	[view1 setAutoresizingMask: NSViewNotSizable];
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+	[window setContentSize: NSMakeSize(50, 100)];
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+	[window setContentSize: NSMakeSize(200, 100)];
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+	[window setContentSize: NSMakeSize(100, 100)];	
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+
+	// NSViewWidthSizable
+
+	[view1 setFrame: NSMakeRect(10,10,10,10)];
+	[view1 setAutoresizingMask: NSViewWidthSizable];
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+	[window setContentSize: NSMakeSize(96, 100)];
+	passed = CHECK(view1, NSMakeRect(10,10,6,10)) && passed;
+	[window setContentSize: NSMakeSize(200, 100)];
+	passed = CHECK(view1, NSMakeRect(10,10,110,10)) && passed;
+	[window setContentSize: NSMakeSize(100, 100)];	
+	passed = CHECK(view1, NSMakeRect(10,10,10,10)) && passed;
+
+	// NSViewWidthSizable | NSViewMaxXMargin
+
+	[view1 setFrame: NSMakeRect(0,0,33,33)];
+	[view1 setAutoresizingMask: NSViewWidthSizable | NSViewMaxXMargin];
+	[window setContentSize: NSMakeSize(200, 100)];
+	passed = CHECK(view1, NSMakeRect(0,0,66,33)) && passed;
+	[window setContentSize: NSMakeSize(100, 100)];
+
+	// NSViewWidthSizable | NSViewMinXMargin
+
+	[view1 setFrame: NSMakeRect(50,0,25,25)];
+	[view1 setAutoresizingMask: NSViewWidthSizable | NSViewMinXMargin];
+	[window setContentSize: NSMakeSize(175, 100)];
+	passed = CHECK(view1, NSMakeRect(100,0,50,25)) && passed;
+	[window setContentSize: NSMakeSize(100, 100)];
+
+
+	pass(passed,"NSView autoresizing works");
+
+	/**
+	 * Rounding test
+	 */
+
+	passed = 1;
+	[view1 setFrame: NSMakeRect(10, 10, 10, 10)];
 	[view1 setAutoresizingMask: NSViewMinXMargin |
 	       NSViewWidthSizable |
 	       NSViewMaxXMargin |
 	       NSViewMinYMargin |
 	       NSViewHeightSizable |
 	       NSViewMaxYMargin ];
-	
-	[[window contentView] addSubview: view1];
 
 	// All autoresize masks are enabled. Check that halving the
 	// width and height of the window works as expected.
