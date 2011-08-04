9d79d0c978e7a52e1c2d117d234df7fe1cd80bc8
Fred Kiefer <fredkiefer@gmx.de>
2011-08-04 08:52:42 +0000

    * Source/NSMenu.m (+initialize): Don't create the default menu   zone as we never use it and a change in base made this an   expensive operation.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@33694 72102866-910b-0410-8b05-ffd578937521

--- Source/NSMenu.m
+++ Source/NSMenu.m
@@ -39,7 +39,6 @@
 #import <Foundation/NSCharacterSet.h>
 #import <Foundation/NSDebug.h>
 #import <Foundation/NSException.h>
-#import <Foundation/NSGarbageCollector.h>
 #import <Foundation/NSProcessInfo.h>
 #import <Foundation/NSString.h>
 #import <Foundation/NSNotification.h>
@@ -578,11 +577,6 @@ static BOOL menuBarVisible = YES;
     {
       [self setVersion: 1];
       nc = [NSNotificationCenter defaultCenter];
-      // If we're not running in GC mode, create a new zone
-      if ([NSGarbageCollector defaultCollector] == nil)
-        {
-          menuZone = NSCreateZone(0, 0, YES);
-        }
     }
 }
 
