0f5b764553386879770ce6cf54d29e385ea6a57c
Jonathan Gillaspie <jonathan.gillaspie@testplant.com>
2012-11-14 01:08:52 +0000

    Add back showsResizeIndicator that got dropped during a merge.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/branches/gnustep_testplant_branch@35792 72102866-910b-0410-8b05-ffd578937521

--- Source/NSWindow.m
+++ Source/NSWindow.m
@@ -2011,10 +2011,7 @@ titleWithRepresentedFilename(NSString *representedFilename)
 
 - (BOOL) showsResizeIndicator
 {
-  // TODO
-  NSLog(@"Method %s is not implemented for class %s",
-        "showsResizeIndicator", "NSWindow");
-  return YES;
+  return ([self styleMask] & NSResizableWindowMask) ? YES : NO;
 }
 
 - (void) setShowsResizeIndicator: (BOOL)show
