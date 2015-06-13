da565d32a75cc58a3be5842173668a6872d05834
Gregory John Casamento <greg.casamento@gmail.com>
2015-06-13 22:09:35 +0000

    Commit header.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/branches/bitmarkets_changes@38632 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/AppKit/NSAnimationContext.h
@@ -0,0 +1,52 @@
+/*
+   NSAnimationContext.h
+ 
+   Created by Gregory John Casamento on Wed Jun 10 2015.
+   Copyright (c) 2015 Free Software Foundation, Inc.
+ 
+   Author: Gregory Casamento <greg.casamento@gmail.com>
+ 
+   This file is part of the GNUstep GUI Library.
+ 
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+ */ 
+
+#ifndef _GNUstep_H_NSAnimationContext_
+#define _GNUstep_H_NSAnimationContext_
+
+#import <Foundation/NSObject.h>
+
+@implementation NSAnimationContext : NSObject
+{
+  void *completionHandler;
+}
+
++ (void) beginGrouping;
++ (void) endGrouping;
++ (NSAnimationContext *)currentContext;
++ (void)runAnimationGroup:(void (^)(NSAnimationContext *context))changes
+        completionHandler:(void (^)(void))completionHandler;
+
+// Properties...
+- (void) setCompletionHandler: (void *)handler;
+- (void *) completionHandler;
+- (void) setTimingFunction: (void *)timingFunction;
+- (void *) timingFunction;
+@end
+
+
+#endif
