d391a290ff06520bae9c2692805817ac05b35261
Gregory John Casamento <greg.casamento@gmail.com>
2016-07-16 22:22:32 +0000

    Remove 'margin'
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/gui/trunk@40003 72102866-910b-0410-8b05-ffd578937521

--- Source/NSView.m
+++ Source/NSView.m
@@ -4240,7 +4240,7 @@ static NSView* findByTag(NSView *view, NSInteger aTag, NSUInteger *level)
   NSSize paperSize = [info paperSize];
   float scale = (double)([self frame].size.width/paperSize.width);
   float xoff = 0;
-  float yoff = ((paperSize.height - self.frame.size.height) - 10) / scale;
+  float yoff = (paperSize.height - self.frame.size.height) / scale;
 
   [ctxt  beginPage: ordinalNum
          label: aString
