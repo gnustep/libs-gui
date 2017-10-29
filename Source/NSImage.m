280c3e0095ffbf2aa53505e9d454fc29abb07884
Ivan Vučica <ivan@vucica.net>
2017-10-29 14:34:02 +0000

    Clean up -[NSImage initWithCoder:], reduce amount of releases of self.
    Some context available in PR #12 and  PR #13.

--- Source/NSImage.m
+++ Source/NSImage.m
@@ -1967,25 +1967,25 @@ static NSSize GSResolutionOfImageRep(NSImageRep *rep)
 - (id) initWithCoder: (NSCoder*)coder
 {
   BOOL flag;
-  NSImage *replacementImage;
-  NSString *imageName;
   
   _reps = [[NSMutableArray alloc] initWithCapacity: 2];
   if ([coder allowsKeyedCoding])
     {
       if ([coder containsValueForKey: @"NSName"])
         {
-          RELEASE(self);
+          NSImage *replacementImage;
+          NSString *imageName;
+
           imageName = [coder decodeObjectForKey: @"NSName"];
           replacementImage = [NSImage imageNamed: imageName];
           if (replacementImage)
             {
+              RELEASE(self);
               return RETAIN(replacementImage);
             }
-          replacementImage = [[NSImage alloc] init];
-          [replacementImage setName: imageName];
-          replacementImage->_flags.archiveByName = YES;
-          self = replacementImage;
+
+          [self setName: imageName];
+          self->_flags.archiveByName = YES;
         }
       if ([coder containsValueForKey: @"NSColor"])
         {
@@ -2062,17 +2062,17 @@ static NSSize GSResolutionOfImageRep(NSImageRep *rep)
       [coder decodeValueOfObjCType: @encode(BOOL) at: &flag];
       if (flag == YES)
         {
+          NSImage *replacementImage;
           NSString *theName = [coder decodeObject];
 
-          RELEASE(self);
           replacementImage = [NSImage imageNamed: theName];
           if (replacementImage)
             {
+              RELEASE(self);
               self = RETAIN(replacementImage);
             }
           else
             {
-              self = [[NSImage alloc] init];
               [self setName: theName];
               self->_flags.archiveByName = YES;
             }
