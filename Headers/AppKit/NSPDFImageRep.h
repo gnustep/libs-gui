efa86f1d11120dcd7f17228972967364e2083f92
Gregory John Casamento <greg.casamento@gmail.com>
2019-11-18 05:23:30 -0500

    Add new files

---
+++ Headers/AppKit/NSPDFImageRep.h
@@ -0,0 +1,61 @@
+/* Definition of class NSPDFImageRep
+   Copyright (C) 2019 Free Software Foundation, Inc.
+   
+   By: heron
+   Date: Fri Nov 15 04:24:27 EST 2019
+
+   This file is part of the GNUstep Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02111 USA.
+*/
+
+#ifndef _NSPDFImageRep_h_GNUSTEP_GUI_INCLUDE
+#define _NSPDFImageRep_h_GNUSTEP_GUI_INCLUDE
+
+#include <AppKit/NSImageRep.h>
+
+#if OS_API_VERSION(MAC_OS_X_VERSION_10_0, GS_API_LATEST)
+
+#if	defined(__cplusplus)
+extern "C" {
+#endif
+
+@interface NSPDFImageRep : NSImageRep
+
++ (instancetype) imageRepWithData: (NSData *)imageData;
+
+- (instancetype) initWithData: (NSData *)imageData;
+
+- (NSRect) bounds;
+- (void) setBounds: (NSRect)bounds;
+
+- (NSInteger) currentPage;
+- (void) setCurrentPage: (NSInteger)currentPage;
+
+- (NSInteger) pageCount;
+
+- (NSData *) PDFRepresentation;
+
+@end
+
+#if	defined(__cplusplus)
+}
+#endif
+
+#endif	/* GS_API_MACOSX */
+
+#endif	/* _NSPDFImageRep_h_GNUSTEP_GUI_INCLUDE */
+
