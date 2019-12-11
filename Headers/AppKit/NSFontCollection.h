9769e652cd8d32e5bc456936aca0659c5799d50c
Gregory John Casamento <greg.casamento@gmail.com>
2019-12-11 00:52:27 -0500

    Header skeleton builds

--- Headers/AppKit/NSFontCollection.h
+++ Headers/AppKit/NSFontCollection.h
@@ -25,17 +25,118 @@
 #ifndef _NSFontCollection_h_GNUSTEP_GUI_INCLUDE
 #define _NSFontCollection_h_GNUSTEP_GUI_INCLUDE
 
-#import <Foundation/NSObject>
+#import <Foundation/NSObject.h>
+#import <AppKit/AppKitDefines.h>
 
-#if OS_API_VERSION(MAC_OS_X_VERSION_10_0, GS_API_LATEST)
+#if OS_API_VERSION(MAC_OS_X_VERSION_10_7, GS_API_LATEST)
 
 #if	defined(__cplusplus)
 extern "C" {
 #endif
+  
+@class NSFontDescriptor, NSLocale, NSError, NSArray, NSDictionary;
 
-@interface NSFontCollection : NSObject
+enum {
+    NSFontCollectionVisibilityProcess = (1UL << 0),
+    NSFontCollectionVisibilityUser = (1UL << 1),
+    NSFontCollectionVisibilityComputer = (1UL << 2)
+};
+typedef NSUInteger NSFontCollectionVisibility;
+
+typedef NSString* NSFontCollectionMatchingOptionKey; 
+
+APPKIT_EXPORT NSFontCollectionMatchingOptionKey const NSFontCollectionIncludeDisabledFontsOption;
+APPKIT_EXPORT NSFontCollectionMatchingOptionKey const NSFontCollectionRemoveDuplicatesOption;
+APPKIT_EXPORT NSFontCollectionMatchingOptionKey const NSFontCollectionDisallowAutoActivationOption;
+
+typedef NSString* NSFontCollectionName;
+
+@interface NSFontCollection : NSObject <NSCopying, NSMutableCopying, NSCoding>
+
+// Initializers...
++ (NSFontCollection *) fontCollectionWithDescriptors: (NSArray *)queryDescriptors;
++ (NSFontCollection *) fontCollectionWithAllAvailableDescriptors;
++ (NSFontCollection *) fontCollectionWithLocale: (NSLocale *)locale;
+
++ (BOOL) showFontCollection: (NSFontCollection *)collection
+                   withName: (NSFontCollectionName)name
+                 visibility: (NSFontCollectionVisibility)visibility
+                      error: (NSError **)error;
+
++ (BOOL) hideFontCollectionWithName: (NSFontCollectionName)name
+                         visibility: (NSFontCollectionVisibility)visibility
+                              error: (NSError **)error;
+
++ (BOOL) renameFontCollectionWithName: (NSFontCollectionName)name
+                           visibility: (NSFontCollectionVisibility)visibility
+                               toName: (NSFontCollectionName)name
+                                error: (NSError **)error;
+
++ (NSArray *) allFontCollectionNames;
+
++ (NSFontCollection *) fontCollectionWithName: (NSFontCollectionName)name;
+
++ (NSFontCollection *) fontCollectionWithName: (NSFontCollectionName)name
+                                   visibility: (NSFontCollectionVisibility)visibility;
+
+
+// Descriptors
+- (NSArray *) queryDescriptors;  // copy
+
+- (NSArray *) exclusionDescriptors;
+
+- (NSArray *) matchingDescriptors; 
+- (NSArray *) matchingDescriptorsWithOptions: (NSDictionary *)options;
+
+- (NSArray *) matchingDescriptorsForFamily: (NSString *)family;
+- (NSArray *) matchingDescriptorsForFamily: (NSString *)family options: (NSDictionary *)options;
+  
+@end
+
+
+@interface NSMutableFontCollection : NSFontCollection
+
++ (NSMutableFontCollection *) fontCollectionWithDescriptors: (NSArray *)queryDescriptors;
++ (NSMutableFontCollection *) fontCollectionWithAllAvailableDescriptors;
++ (NSMutableFontCollection *) fontCollectionWithLocale: (NSLocale *)locale;
++ (NSMutableFontCollection *) fontCollectionWithName: (NSFontCollectionName)name;
+
++ (NSMutableFontCollection *) fontCollectionWithName: (NSFontCollectionName)name
+                                          visibility: (NSFontCollectionVisibility)visibility;
+
+- (NSArray *) queryDescriptors;  // copy
+- (void) setQueryDescriptors: (NSArray *)queryDescriptors;  // copy
+
+- (NSArray *) exclusionDescriptors;
+- (void) setExclusionDescriptors: (NSArray *)exclusionDescriptors;
+
+- (void)addQueryForDescriptors: (NSArray *)descriptors;
+
+- (void)removeQueryForDescriptors: (NSArray *)descriptors;
 
 @end
+
+// NSFontCollectionDidChangeNotification
+APPKIT_EXPORT NSString * const NSFontCollectionDidChangeNotification;
+
+// Notification user info dictionary keys
+typedef NSString * NSFontCollectionUserInfoKey;
+APPKIT_EXPORT NSFontCollectionUserInfoKey const NSFontCollectionActionKey;
+APPKIT_EXPORT NSFontCollectionUserInfoKey const NSFontCollectionNameKey;
+APPKIT_EXPORT NSFontCollectionUserInfoKey const NSFontCollectionOldNameKey;
+APPKIT_EXPORT NSFontCollectionUserInfoKey const NSFontCollectionVisibilityKey;
+
+// Values for NSFontCollectionAction
+typedef NSString * NSFontCollectionActionTypeKey;
+APPKIT_EXPORT NSFontCollectionActionTypeKey const NSFontCollectionWasShown;
+APPKIT_EXPORT NSFontCollectionActionTypeKey const NSFontCollectionWasHidden;
+APPKIT_EXPORT NSFontCollectionActionTypeKey const NSFontCollectionWasRenamed;
+
+// Standard named collections
+APPKIT_EXPORT NSFontCollectionName const NSFontCollectionAllFonts;
+APPKIT_EXPORT NSFontCollectionName const NSFontCollectionUser;
+APPKIT_EXPORT NSFontCollectionName const NSFontCollectionFavorites;
+APPKIT_EXPORT NSFontCollectionName const NSFontCollectionRecentlyUsed;
 
 #if	defined(__cplusplus)
 }
