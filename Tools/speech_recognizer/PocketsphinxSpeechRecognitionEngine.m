20fefae849e46ccd90c7e863559bcad831e571ce
Gregory John Casamento <greg.casamento@gmail.com>
2020-02-03 07:56:55 -0500

    Fix link errors

--- Tools/speech_recognizer/PocketsphinxSpeechRecognitionEngine.m
+++ Tools/speech_recognizer/PocketsphinxSpeechRecognitionEngine.m
@@ -5,19 +5,44 @@
  * Implementation of a speech engine using pocketsphinx.  This should be the default
  * for resource-constrained platforms.
  */
+
+#define MODELDIR "/share/pocketsphinx/model"
+
+/* 
+    ps_decoder_t *ps = NULL;
+    cmd_ln_t *config = NULL;
+
+    config = cmd_ln_init(NULL, ps_args(), TRUE,
+		         "-hmm", MODELDIR "/en-us/en-us",
+	                 "-lm", MODELDIR "/en-us/en-us.lm.bin",
+	                 "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+	                 NULL);
+
+ */
 @interface PocketsphinxSpeechRecognitionEngine : GSSpeechRecognitionEngine
 {
+  ps_decoder_t *ps;
+  cmd_ln_t *config;
 }
 @end
 
 @implementation PocketsphinxSpeechRecognitionEngine
+
 + (void)initialize
 {
 }
 
 - (id)init
 {
-  if (nil == (self = [super init])) { return nil; }
+  if (nil != (self = [super init]))
+    {
+      config = cmd_ln_init(NULL, ps_args(), TRUE,
+                           "-hmm", MODELDIR "/en-us/en-us",
+                           "-lm", MODELDIR "/en-us/en-us.lm.bin",
+                           "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+                           NULL);
+      ps = ps_init(config);
+    }
   return self;
 }
 
@@ -32,8 +57,10 @@
 @end
 
 @implementation GSSpeechRecognitionEngine (Pocketsphinx)
+
 + (GSSpeechRecognitionEngine*)defaultSpeechRecognitionEngine
 {
   return [[[PocketsphinxSpeechRecognitionEngine alloc] init] autorelease];
 }
+
 @end
