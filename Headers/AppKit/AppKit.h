a8cf81626b2df6798e239431a304d5783bd91b27
Gregory John Casamento <greg.casamento@gmail.com>
2021-02-25 06:51:05 -0500

    Remove NSStackView from branch

--- Headers/AppKit/AppKit.h
+++ Headers/AppKit/AppKit.h
@@ -228,7 +228,6 @@
 #import <AppKit/NSSound.h>
 #import <AppKit/NSSpeechRecognizer.h>
 #import <AppKit/NSSpeechSynthesizer.h>
-#import <AppKit/NSStackView.h>
 #import <AppKit/NSStepperTouchBarItem.h>
 #import <AppKit/NSStepper.h>
 #import <AppKit/NSStepperCell.h>
