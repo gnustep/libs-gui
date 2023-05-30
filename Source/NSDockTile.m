ec32d87021ae1693c5addb9be4510f90f588d294
Gregory John Casamento <greg.casamento@gmail.com>
2023-05-30 05:26:51 -0400

    Fix accidental error

--- Source/NSDockTile.m
+++ Source/NSDockTile.m
@@ -1,4 +1,4 @@
-gg/* Implementation of class NSDockTile
+/* Implementation of class NSDockTile
    Copyright (C) 2019 Free Software Foundation, Inc.
 
    By: Gregory John Casamento <greg.casamento@gmail.com>
