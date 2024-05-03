bd4e093bad6dd3102de616072fe223c22725f586
Gregory John Casamento <greg.casamento@gmail.com>
2024-05-03 13:52:23 -0400

    Add NSMenuToolbarItem skeleton

---
+++ Source/NSMenuToolbarItem.m
@@ -0,0 +1,68 @@
+/* Implementation of class NSMenuToolbarItem
+   Copyright (C) 2024 Free Software Foundation, Inc.
+   
+   By: Gregory John Casamento
+   Date: 03-05-2024
+
+   This file is part of the GNUstep Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110 USA.
+*/
+
+#import "AppKit/NSMenuToolbarItem.h"
+#import "AppKit/NSMenu.h"
+
+@implementation NSMenuToolbarItem
+
+- (void) dealloc
+{
+  RELEASE(_menu);
+  RELEASE(_itemMenu);
+  [super dealloc];
+}
+
+- (BOOL) showsIndicator
+{
+  return _showsIndicator;
+}
+
+- (void) setShowsIndicator: (BOOL)flag
+{
+  _showsIndicator = flag;
+}
+
+- (NSMenu *) menu
+{
+  return _menu;
+}
+
+- (void) setMenu: (NSMenu *)menu
+{
+  ASSIGN(_menu, menu);
+}
+
+- (NSMenu *) itemMenu
+{
+  return _itemMenu;
+}
+
+- (void) setItemMenu: (NSMenu *)itemMenu
+{
+  ASSIGN(_itemMenu, itemMenu);
+}
+
+@end
+
