27c9dc92072e671c16994eb0ad6ab411128be72a
Gregory John Casamento <greg.casamento@gmail.com>
2024-05-06 10:09:39 -0400

    Fix whitespace in NSTableRowView.[hm]

--- Headers/AppKit/NSTableRowView.h
+++ Headers/AppKit/NSTableRowView.h
@@ -1,21 +1,21 @@
 /* Definition of class NSTableRowView
    Copyright (C) 2022 Free Software Foundation, Inc.
-   
+
    By: Gregory John Casamento <greg.casamento@gmail.com>
    Date: 03-09-2022
 
    This file is part of the GNUstep Library.
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2.1 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, write to the Free
    Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
@@ -90,7 +90,7 @@ extern "C" {
 - (NSInteger) numberOfColumns;
 
 - (NSColor *) backgroundColor;
-- (void) setBackgroundColor: (NSColor *)color;  
+- (void) setBackgroundColor: (NSColor *)color;
 
 - (void) drawBackgroundInRect: (NSRect)dirtyRect;
 
@@ -107,7 +107,7 @@ extern "C" {
 
 - (BOOL) isPreviousRowSelected;
 - (void) setPreviousRowSelected: (BOOL)flag;
-  
+
 @end
 
 #if	defined(__cplusplus)
