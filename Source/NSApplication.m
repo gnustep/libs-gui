1fd80dabc25a197c88014c62582c58a6d490e53d
williameveretteggplant <44709277+williameveretteggplant@users.noreply.github.com>
2024-08-07 16:48:39 -0600

    Force modal to front

--- Source/NSApplication.m
+++ Source/NSApplication.m
@@ -1785,6 +1785,7 @@ static BOOL _isAutolaunchChecked = NO;
 	  /*
 	   * Try to handle events for this session, discarding others.
 	   */
+    	  [theSession->window orderFrontRegardless];
 	  code = [self runModalSession: theSession];
 	  if (code == NSRunContinuesResponse)
 	    {
