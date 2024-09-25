44a42ddc03b55ba595b6755e6a6e5bf8f869d5f2
Frederick John Milens III <frederick@milens3.com>
2024-09-24 22:48:09 -0500

    Update test case

--- Tests/gui/NSParagraphStyle/NSParagraphStyle_equalityTesting.m
+++ Tests/gui/NSParagraphStyle/NSParagraphStyle_equalityTesting.m
@@ -5,9 +5,9 @@
 #include <AppKit/NSTextList.h>
 
 int main(int argc, char **argv)
-{  
+{    
   CREATE_AUTORELEASE_POOL(arp);
-  
+    
   START_SET("NSParagraphStyle equality tests");
   
   NSMutableParagraphStyle *default1 = [NSParagraphStyle defaultParagraphStyle];
@@ -17,6 +17,8 @@ int main(int argc, char **argv)
   
   NSMutableParagraphStyle *style1 = [[NSMutableParagraphStyle alloc] init];
   NSMutableParagraphStyle *style2 = [[NSMutableParagraphStyle alloc] init];
+  
+  PASS_EQUAL(style1, style2, "NSParagraphStyle isEqual: works for default mutable copies");
   
   NSTextList *textList = [[NSTextList alloc] init];
   
@@ -29,7 +31,7 @@ int main(int argc, char **argv)
   [style2 setTextLists: [NSArray arrayWithObject: [[NSTextList alloc] init]]];
   
   pass(![style1 isEqual: style2], "NSParagraphStyle isEqual: works for different textlists");
-
+  
   END_SET("NSParagraphStyle equality tests");
   
   DESTROY(arp);
