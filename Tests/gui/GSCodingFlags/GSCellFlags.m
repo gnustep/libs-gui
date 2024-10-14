7797316d827a27279c41a425f581ce621512781a
Johnathan Becker <john.becker@keysight.com>
2024-10-14 14:28:38 -0600

    fix =

--- Tests/gui/GSCodingFlags/GSCellFlags.m
+++ Tests/gui/GSCodingFlags/GSCellFlags.m
@@ -36,13 +36,13 @@ int main()
     mask.value = 0b10010000001110000000000001001000;
 #endif
 
-    pass(mask.flags.state = 1, "state is correctly set");
-    pass(mask.flags.selectable = 1, "selectable is correctly set");
-    pass(mask.flags.scrollable = 1, "scrollable is correctly set");
-    pass(mask.flags.editable = 1, "editable is correctly set");
-    pass(mask.flags.continuous = 1, "continuous is correctly set");
-    pass(mask.flags.useUserKeyEquivalent = 1, "useUserKeyEquivalent is correctly set");
-    pass(mask.flags.truncateLastLine = 1, "truncateLastLine is correctly set");
+    pass(mask.flags.state == 1, "state is correctly set");
+    pass(mask.flags.selectable == 1, "selectable is correctly set");
+    pass(mask.flags.scrollable == 1, "scrollable is correctly set");
+    pass(mask.flags.editable == 1, "editable is correctly set");
+    pass(mask.flags.continuous == 1, "continuous is correctly set");
+    pass(mask.flags.useUserKeyEquivalent == 1, "useUserKeyEquivalent is correctly set");
+    pass(mask.flags.truncateLastLine == 1, "truncateLastLine is correctly set");
     
     END_SET("GSCodingFlags GNUstep CellFlags Union")
 
