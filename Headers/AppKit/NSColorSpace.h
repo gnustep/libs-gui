944f9237b77a125d92eb58122caf88dc2d6b9ee2
Gregory John Casamento <greg.casamento@gmail.com>
2025-07-26 15:19:13 -0400

    Add NSColorSpace documentation

--- Headers/AppKit/NSColorSpace.h
+++ Headers/AppKit/NSColorSpace.h
@@ -37,6 +37,16 @@
 @class NSData;
 @class NSString;
 
+/**
+ * NSColorSpace represents and manages color space information used
+ * in color conversions and rendering. It supports standard and device-specific
+ * color spaces and enables loading color profiles via ICC data or
+ * ColorSync profiles.
+ *
+ * The class provides factory methods to access common color spaces,
+ * including generic and device RGB, CMYK, and grayscale spaces. It also
+ * allows inspection of color space models and profile data.
+ */
 typedef enum _NSColorSpaceModel
 {
   NSUnknownColorSpaceModel = -1,
@@ -61,19 +71,69 @@ APPKIT_EXPORT_CLASS
   void *_colorSyncProfile;
 }
 
+/**
+ * Returns the device CMYK color space.
+ */
 + (NSColorSpace *)deviceCMYKColorSpace;
+
+/**
+ * Returns the device grayscale color space.
+ */
 + (NSColorSpace *)deviceGrayColorSpace;
+
+/**
+ * Returns the device RGB color space.
+ */
 + (NSColorSpace *)deviceRGBColorSpace;
+
+/**
+ * Returns the generic CMYK color space.
+ */
 + (NSColorSpace *)genericCMYKColorSpace;
+
+/**
+ * Returns the generic grayscale color space.
+ */
 + (NSColorSpace *)genericGrayColorSpace;
+
+/**
+ * Returns the generic RGB color space.
+ */
 + (NSColorSpace *)genericRGBColorSpace;
 
+/**
+ * Returns the color space model associated with this instance.
+ */
 - (NSColorSpaceModel)colorSpaceModel;
+
+/**
+ * Returns the ColorSync profile associated with this color space.
+ */
 - (void *)colorSyncProfile;
+
+/**
+ * Returns the ICC profile data associated with this color space.
+ */
 - (NSData *)ICCProfileData;
+
+/**
+ * Initializes the color space with a specified ColorSync profile.
+ */
 - (id)initWithColorSyncProfile:(void *)prof;
+
+/**
+ * Initializes the color space using ICC profile data.
+ */
 - (id)initWithICCProfileData:(NSData *)iccData;
+
+/**
+ * Returns a localized, human-readable name for this color space.
+ */
 - (NSString *)localizedName;
+
+/**
+ * Returns the number of color components in this color space.
+ */
 - (int)numberOfColorComponents;
 
 @end
