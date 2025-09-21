35d2c52276c7465f88ef22c44d2fa2466d7ca1c2
Gregory John Casamento <greg.casamento@gmail.com>
2025-09-21 12:09:36 -0400

    Add rotation recognizer

--- Headers/AppKit/NSRotationGestureRecognizer.h
+++ Headers/AppKit/NSRotationGestureRecognizer.h
@@ -1,25 +1,25 @@
 /* Definition of class NSRotationGestureRecognizer
    Copyright (C) 2019 Free Software Foundation, Inc.
-   
+
    By: Gregory John Casamento
    Date: Thu Dec  5 12:54:21 EST 2019
 
    This file is part of the GNUstep Library.
-   
+
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
-   
+
    This library is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    Lesser General Public License for more details.
-   
+
    You should have received a copy of the GNU Lesser General Public
    License along with this library; see the file COPYING.LIB.
-   If not, see <http://www.gnu.org/licenses/> or write to the 
-   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   If not, see <http://www.gnu.org/licenses/> or write to the
+   Free Software Foundation, 51 Franklin Street, Fifth Floor,
    Boston, MA 02110-1301, USA.
 */
 
@@ -27,6 +27,7 @@
 #define _NSRotationGestureRecognizer_h_GNUSTEP_GUI_INCLUDE
 
 #import <AppKit/NSGestureRecognizer.h>
+#import <Foundation/NSTimer.h>
 
 #if OS_API_VERSION(MAC_OS_X_VERSION_10_0, GS_API_LATEST)
 
@@ -36,6 +37,23 @@ extern "C" {
 
 APPKIT_EXPORT_CLASS
 @interface NSRotationGestureRecognizer : NSGestureRecognizer
+{
+@private
+    CGFloat _rotation;
+    CGFloat _velocity;
+    NSPoint _initialLocation;
+    NSPoint _currentLocation;
+    CGFloat _initialAngle;
+    CGFloat _currentAngle;
+    NSTimeInterval _initialTime;
+    NSTimeInterval _currentTime;
+    BOOL _rotationStarted;
+}
+
+// Getting the Rotation Values
+- (CGFloat) rotation;
+
+- (CGFloat) velocity;
 
 @end
 
