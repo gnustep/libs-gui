dd575e64e7bfff8e433308a585b74e80d8c7640e
Gregory Casamento <greg.casamento@gmail.com>
2026-02-19 19:32:22 -0500

    Add missing files.

---
+++ Headers/AppKit/GSViewAccessibilityData.h
@@ -0,0 +1,100 @@
+/** <title>GSViewAccessibilityData</title>
+
+   <abstract>Encapsulates accessibility properties for NSView</abstract>
+
+   Copyright (C) 2026 Free Software Foundation, Inc.
+
+   This file is part of the GNUstep GUI Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef _GNUstep_H_GSViewAccessibilityData
+#define _GNUstep_H_GSViewAccessibilityData
+
+#import <Foundation/NSObject.h>
+#import <AppKit/NSAccessibilityConstants.h>
+
+@class NSString;
+@class NSArray;
+
+/**
+ * GSViewAccessibilityData encapsulates all accessibility-related properties
+ * for NSView instances. This allows views to only allocate this object when
+ * accessibility functionality is actually needed, reducing memory overhead.
+ */
+@interface GSViewAccessibilityData : NSObject
+{
+@private
+  NSString *_accessibilityLabel;
+  NSString *_accessibilityValue;
+  NSString *_accessibilityHelp;
+  NSAccessibilityRole _accessibilityRole;
+  NSString *_accessibilityTitle;
+  NSString *_accessibilityRoleDescription;
+  NSString *_accessibilityIdentifier;
+  NSArray *_accessibilityUserInputLabels;
+  NSArray *_accessibilityChildren;
+  NSArray *_accessibilityCustomActions;
+  id _accessibilityParent;            // weak reference
+  BOOL _accessibilityFocused;
+  BOOL _accessibilityEnabled;
+}
+
+// Property accessors
+- (NSString *) accessibilityLabel;
+- (void) setAccessibilityLabel: (NSString *)label;
+
+- (NSString *) accessibilityValue;
+- (void) setAccessibilityValue: (NSString *)value;
+
+- (NSString *) accessibilityHelp;
+- (void) setAccessibilityHelp: (NSString *)help;
+
+- (NSAccessibilityRole) accessibilityRole;
+- (void) setAccessibilityRole: (NSAccessibilityRole)role;
+
+- (NSString *) accessibilityTitle;
+- (void) setAccessibilityTitle: (NSString *)title;
+
+- (NSString *) accessibilityRoleDescription;
+- (void) setAccessibilityRoleDescription: (NSString *)roleDescription;
+
+- (NSString *) accessibilityIdentifier;
+- (void) setAccessibilityIdentifier: (NSString *)identifier;
+
+- (NSArray *) accessibilityUserInputLabels;
+- (void) setAccessibilityUserInputLabels: (NSArray *)labels;
+
+- (NSArray *) accessibilityChildren;
+- (void) setAccessibilityChildren: (NSArray *)children;
+
+- (NSArray *) accessibilityCustomActions;
+- (void) setAccessibilityCustomActions: (NSArray *)actions;
+
+- (id) accessibilityParent;
+- (void) setAccessibilityParent: (id)parent;
+
+- (BOOL) isAccessibilityFocused;
+- (void) setAccessibilityFocused: (BOOL)focused;
+
+- (BOOL) isAccessibilityEnabled;
+- (void) setAccessibilityEnabled: (BOOL)enabled;
+
+@end
+
+#endif // _GNUstep_H_GSViewAccessibilityData
---
+++ Source/GSViewAccessibilityData.m
@@ -0,0 +1,180 @@
+/** <title>GSViewAccessibilityData</title>
+
+   <abstract>Encapsulates accessibility properties for NSView</abstract>
+
+   Copyright (C) 2026 Free Software Foundation, Inc.
+
+   This file is part of the GNUstep GUI Library.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#import <AppKit/GSViewAccessibilityData.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSArray.h>
+
+@implementation GSViewAccessibilityData
+
+- (void) dealloc
+{
+  RELEASE(_accessibilityLabel);
+  RELEASE(_accessibilityValue);
+  RELEASE(_accessibilityHelp);
+  RELEASE(_accessibilityRole);
+  RELEASE(_accessibilityTitle);
+  RELEASE(_accessibilityRoleDescription);
+  RELEASE(_accessibilityIdentifier);
+  RELEASE(_accessibilityUserInputLabels);
+  RELEASE(_accessibilityChildren);
+  RELEASE(_accessibilityCustomActions);
+  // _accessibilityParent is a weak reference, no RELEASE needed
+  
+  [super dealloc];
+}
+
+- (NSString *) accessibilityLabel
+{
+  return _accessibilityLabel;
+}
+
+- (void) setAccessibilityLabel: (NSString *)label
+{
+  ASSIGN(_accessibilityLabel, label);
+}
+
+- (NSString *) accessibilityValue
+{
+  return _accessibilityValue;
+}
+
+- (void) setAccessibilityValue: (NSString *)value
+{
+  ASSIGN(_accessibilityValue, value);
+}
+
+- (NSString *) accessibilityHelp
+{
+  return _accessibilityHelp;
+}
+
+- (void) setAccessibilityHelp: (NSString *)help
+{
+  ASSIGN(_accessibilityHelp, help);
+}
+
+- (NSAccessibilityRole) accessibilityRole
+{
+  return _accessibilityRole;
+}
+
+- (void) setAccessibilityRole: (NSAccessibilityRole)role
+{
+  ASSIGN(_accessibilityRole, role);
+}
+
+- (NSString *) accessibilityTitle
+{
+  return _accessibilityTitle;
+}
+
+- (void) setAccessibilityTitle: (NSString *)title
+{
+  ASSIGN(_accessibilityTitle, title);
+}
+
+- (NSString *) accessibilityRoleDescription
+{
+  return _accessibilityRoleDescription;
+}
+
+- (void) setAccessibilityRoleDescription: (NSString *)roleDescription
+{
+  ASSIGN(_accessibilityRoleDescription, roleDescription);
+}
+
+- (NSString *) accessibilityIdentifier
+{
+  return _accessibilityIdentifier;
+}
+
+- (void) setAccessibilityIdentifier: (NSString *)identifier
+{
+  ASSIGN(_accessibilityIdentifier, identifier);
+}
+
+- (NSArray *) accessibilityUserInputLabels
+{
+  return _accessibilityUserInputLabels;
+}
+
+- (void) setAccessibilityUserInputLabels: (NSArray *)labels
+{
+  ASSIGN(_accessibilityUserInputLabels, labels);
+}
+
+- (NSArray *) accessibilityChildren
+{
+  return _accessibilityChildren;
+}
+
+- (void) setAccessibilityChildren: (NSArray *)children
+{
+  ASSIGN(_accessibilityChildren, children);
+}
+
+- (NSArray *) accessibilityCustomActions
+{
+  return _accessibilityCustomActions;
+}
+
+- (void) setAccessibilityCustomActions: (NSArray *)actions
+{
+  ASSIGN(_accessibilityCustomActions, actions);
+}
+
+- (id) accessibilityParent
+{
+  return _accessibilityParent;
+}
+
+- (void) setAccessibilityParent: (id)parent
+{
+  // Weak reference - don't retain
+  _accessibilityParent = parent;
+}
+
+- (BOOL) isAccessibilityFocused
+{
+  return _accessibilityFocused;
+}
+
+- (void) setAccessibilityFocused: (BOOL)focused
+{
+  _accessibilityFocused = focused;
+}
+
+- (BOOL) isAccessibilityEnabled
+{
+  return _accessibilityEnabled;
+}
+
+- (void) setAccessibilityEnabled: (BOOL)enabled
+{
+  _accessibilityEnabled = enabled;
+}
+
+@end
