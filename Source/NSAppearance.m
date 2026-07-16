3c52fcb7745b28d28a15f91251bdb8a66f585782
Todd White <todd.white@thalion.global>
2026-07-16 09:48:11 -0400

    Fix: there is always a current appearance
    currentAppearance returned nil until something set one, so a view or the application asked for its effective appearance answered nil.  AppKit reports aqua on a machine that has not been told otherwise.

--- Source/NSAppearance.m
+++ Source/NSAppearance.m
@@ -97,6 +97,12 @@ NSAppearance *__currentAppearance = nil;
 
 + (NSAppearance *) currentAppearance
 {
+  if (__currentAppearance == nil)
+    {
+      __currentAppearance = [[NSAppearance alloc]
+        initWithAppearanceNamed: NSAppearanceNameAqua
+                         bundle: nil];
+    }
   return __currentAppearance;
 }
 
