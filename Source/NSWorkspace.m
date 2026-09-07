7984394a05ff0b2a3c5cbde210facb7f8a21b9bd
Riccardo Mottola <rm@gnu.org>
2026-09-08 00:59:15 +0200

    rename variable from imgDir to picDir reflecting that it contains NSPicturesDirectory

--- Source/NSWorkspace.m
+++ Source/NSWorkspace.m
@@ -764,7 +764,7 @@ static NSDictionary		*urlPreferences = nil;
   NSArray *appDirs;
   NSArray *downloadDir;
   NSArray *desktopDir;
-  NSArray *imgDir;
+  NSArray *picDir;
   NSArray *musicDir;
   NSArray *videoDir;
   NSString *sysDir;
@@ -819,7 +819,7 @@ static NSDictionary		*urlPreferences = nil;
     NSSystemDomainMask, YES);
   appDirs = NSSearchPathForDirectoriesInDomains(NSApplicationDirectory,
     NSAllDomainsMask, YES);
-  imgDir = NSSearchPathForDirectoriesInDomains(NSPicturesDirectory,
+  picDir = NSSearchPathForDirectoriesInDomains(NSPicturesDirectory,
     NSUserDomainMask, YES);
   musicDir = NSSearchPathForDirectoriesInDomains(NSMusicDirectory,
     NSUserDomainMask, YES);
@@ -869,10 +869,10 @@ static NSDictionary		*urlPreferences = nil;
       [folderPathIconDict setObject: @"Desktop"
 	forKey: [[desktopDir objectAtIndex: i] stringByResolvingSymlinksInPath]];
     }
-  for (i = 0; i < [imgDir count]; i++)
+  for (i = 0; i < [picDir count]; i++)
     {
       [folderPathIconDict setObject: @"ImageFolder"
-	forKey: [[imgDir objectAtIndex: i] stringByResolvingSymlinksInPath]];
+	forKey: [[picDir objectAtIndex: i] stringByResolvingSymlinksInPath]];
     }
   for (i = 0; i < [musicDir count]; i++)
     {
